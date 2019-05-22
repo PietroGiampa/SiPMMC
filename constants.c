0d411dc5674fc3b8489b34c59175d97bbc859317
Frankie Polak <epolak@triumf.ca>
2019-05-22 13:19:45 -0700

    Added new constant file

--- constants.c
+++ constants.c
@@ -7,10 +7,10 @@ double TPB_fast_t = 10.0; //TPB Fast time constant [ns]
 double TPB_int_t = 49.0; //TPB intermediate time constant [ns]
 double TPB_long_t = 3550.0; //TPB long time constant [ns]
 double TPB_spu_t = 309.0; //TPB spurious time constant [ns]
-double TPB_fast_r = 60.0;
-double TPB_int_r = 30.0;
-double TPB_long_r = 8.0;
-double TPB_spu_r = 2.0;
+double TPB_fast_r = 60.0; //abundance percentage
+double TPB_int_r = 30.0; //abundance percentage
+double TPB_long_r = 8.0; //abundance percentage
+double TPB_spu_r = 2.0; //abundance percentage
 TRandom3 rnd;
 TRandom3 rndCDP;
 double overvoltage = 4.0; //Set the SiPM overvoltage [V]
@@ -20,5 +20,5 @@ double time_trigger = 2000.0; //DAQ Trigger time [ns]
 double time_full_window = time_window+time_trigger; //DAQ full window [ns]
 //double pdePDF = 0.2; //PDE for the SIPM, assumed to be flat between 100-300 [nm]
 double DN_rate = 0.08; //Hz/mm2
-double n_SiPM = 216; //number of SiPM in the epxeriment
-double SiPM_A = 25; //mm2 
+double n_SiPM = 216; //number of SiPM in the experiment
+double SiPM_A = 25; //mm2

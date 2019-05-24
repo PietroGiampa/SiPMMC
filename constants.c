67ec679eff87e6463cef0d667c93ad4ef606e2b1
Frankie Polak <epolak@triumf.ca>
2019-05-24 16:59:54 -0700

    changed the number of SiPMs to 30 and added sources for the constants

--- constants.c
+++ constants.c
@@ -1,8 +1,7 @@
 //Define all the variables
 
-//Where are these numbers from?
+//https://arxiv.org/abs/1403.0525
 double photo_yield = 40.0; //LAr photo yield [photons/keV]
-double coll_eff = 1.0; //detector collection efficiency [%]
 double LAr_fast_t = 7.0; //LAr fast scintillation time const [ns]
 double LAr_slow_t = 1600.0; //LAr slow scintillation time const [ns]
 
@@ -17,9 +16,11 @@ double TPB_long_r = 0.08; //abundance
 double TPB_spu_r = 0.02; //abundance (abundances should add up to 1)
 
 //https://arxiv.org/abs/1903.03663
-double pde = 0.125; //PDE for the SIPM at 189 nm
-double DN_rate = 0.2; //Hz/mm^2
+double pde = 0.125; //PDE for the Hamamatsu VUV4 SIPM at 189 nm
+double DN_rate = 0.2; //Hz/mm^2, for the Hamamatsu VUV4
 
+// set by user
+double coll_eff = 1.0; //detector collection efficiency [%] - I think this might be a simplification that there's 100% area coverage, which we'll update later
 TRandom3 rnd;
 TRandom3 rndCDP;
 double overvoltage = 4.0; //Set the SiPM overvoltage [V]
@@ -27,7 +28,7 @@ double surfA = 100.0; //SiPM surface area [mm2]
 double time_window = 6000.0; //DAQ recording window [ns]
 double time_trigger = 2000.0; //DAQ Trigger time [ns]
 double time_full_window = time_window+time_trigger; //DAQ full window [ns]
-double n_SiPM = 216; //number of SiPM in the experiment
+double n_SiPM = 30; //number of SiPMs in the experiment
 double SiPM_A = 25; //area in mm2
 double energy_min = 4.0; //minimum event energy in keV
 double energy_max = 30.0; //maximum event energy in keV

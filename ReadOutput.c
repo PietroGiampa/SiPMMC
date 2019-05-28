c5bdbd51908e2463d1f78f9370e66d046cdbb0ed
Frankie Polak <epolak@triumf.ca>
2019-05-28 16:48:53 -0700

    trying to figure out ReadOutput.c

--- ReadOutput.c
+++ ReadOutput.c
@@ -1,14 +1,13 @@
-void ReadOutput(filename){
-  //PSD vs energy
-  varexp1 = epsd:erecoil
-  selection1 = ?
-  option1 = "AP"
+void ReadOutput(const char * filename){
+  //copied from https://root.cern.ch/doc/v612/ntuple1_8C.html-----------//
+  gInterpreter->LoadMacro(filename);
+  //--------------------------------------------------------------------//
 
-  TTree::Draw(*varexp1, *selection1, *option1, nevents1, firstevent1);
-
-  //# of collected photons vs total number of events
-  varexp2 = n_coll_p:ievt
-  selection2 = ?
-  option2 = "AP"
-  TTree::Draw(*varexp2, *selection2, *option2, nevents2, firstevent2);
+//  TCanvas *c1 = new TCanvas("c1","The Ntuple canvas",200,10,700,780);
+//
+//  //PSD vs energy
+//  const char varexp1 = epsd:erecoil
+//  const char selection1 = ?
+//  Option_t option1 = "AP"
+//  TTree::Draw(*varexp1, *selection1, *option1);
 }

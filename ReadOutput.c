d92b0203b38dd3ee0dda1addb4e9526fdf9bd3e9
Frankie Polak <epolak@triumf.ca>
2019-05-30 16:45:32 -0700

    combined the three for loops in ReadOutput.c

--- ReadOutput.c
+++ ReadOutput.c
@@ -4,9 +4,11 @@
 
 void ReadOutput(TString filename){
 
+  //read in the data file
   TFile *fileIN = TFile::Open(filename);
   TTree *SiPMmc = (TTree*)fileIN->Get("SiPMmc");
 
+  //fetch the necessary stuff from the data file
   Double_t epsd, rpsd, erecoil;
   Int_t n_coll_p;
   SiPMmc->SetBranchAddress("epsd",&epsd);
@@ -20,17 +22,33 @@ void ReadOutput(TString filename){
   //remove the .root
   filename.ReplaceAll(".root","");
 
+  //get rid of the stats box
+  gStyle->SetOptStat(0);
+
+
   // Graph1: true PSD vs recorded PSD
   //making a graph of 100 bins from 0 to 1 on the x axis and 100 bins from 0 to 1 on the y axis
   TH2D *hPSD = new TH2D("hPSD","",100,0,1,100,0,1);
+
+  //Graph2: detected photons vs energy
+  //not sure what the maximum photon number should be
+  TH2D *hPhotons = new TH2D("hphotons", "", 100, energy_min, energy_max, 100, 0, 1300);
+
+  //Graph3: recorded PSD vs energy
+  TH2D *hPSDenergy = new TH2D("hPSDenergy", "", 100, energy_min, energy_max, 100, 0, 1);
+
 
   //loop through all of the events and add them to the graph
   for (int i=0; i<evt_max; i++){
      SiPMmc->GetEntry(i);
      hPSD->Fill(rpsd,epsd);
+     hPhotons->Fill(erecoil,n_coll_p);
+     hPSDenergy->Fill(erecoil,rpsd);
   }
 
+
   //graph the data
+  //Graph1
   TCanvas *c1 = new TCanvas("c1","c1");
   hPSD->SetTitle(filename);
   hPSD->GetXaxis()->SetTitle("Recorded PSD");
@@ -43,35 +61,19 @@ void ReadOutput(TString filename){
   //save the image
   c1->SaveAs("Img/"+filename+"__TruePSDvsRecPSD.png");
 
-
-  //Graph2: detected photons vs energy
-  //not sure what the maximum photon number should be
-  TH2D *hPhotons = new TH2D("hphotons", "", 100, energy_min, energy_max, 100, 0, 1300);
-
-  for (int i=0;i<evt_max;i++){
-    SiPMmc->GetEntry(i);
-    hPhotons->Fill(erecoil,n_coll_p);
-  }
-
+  //Graph2
   TCanvas *c2 = new TCanvas("c2","c2");
   hPhotons->SetTitle(filename);
-  hPhotons->GetXaxis()->SetTitle("Recoil energy");
+  hPhotons->GetXaxis()->SetTitle("Recoil energy (keV)");
   hPhotons->GetYaxis()->SetTitle("Number of collected photons");
   hPhotons->Draw();
 
   c2->SaveAs("Img/"+filename+"__PhotonsVsEnergy.png");
 
-  //Graph3: recorded PSD vs energy
-  TH2D *hPSDenergy = new TH2D("hPSDenergy", "", 100, energy_min, energy_max, 100, 0, 1);
-
-  for (int i=0;i<evt_max;i++){
-    SiPMmc->GetEntry(i);
-    hPSDenergy->Fill(erecoil,rpsd);
-  }
-
+  //Graph3
   TCanvas *c3 = new TCanvas("c3","c3");
   hPSDenergy->SetTitle(filename);
-  hPSDenergy->GetXaxis()->SetTitle("Recoil energy");
+  hPSDenergy->GetXaxis()->SetTitle("Recoil energy (keV)");
   hPSDenergy->GetYaxis()->SetTitle("Recorded PSD");
   hPSDenergy->Draw();
 

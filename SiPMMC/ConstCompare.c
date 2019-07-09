36638edc3a6cfbf7b7788a93bbcbf0e9e1a5d895
Frankie Polak <frankie.polak@gmail.com>
2019-07-09 10:28:11 -0700

    this is the old master, I haven't changed anything yet

--- SiPMMC/ConstCompare.c
+++ SiPMMC/ConstCompare.c
@@ -6,10 +6,9 @@
 //currently have enough colours for 14 files
 
 #include "MakeList.c"
-//#include <math.h>
-#include "nr50.c"
+#include <math.h>
 
-void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""){
+void ConstCompare(int len, TString files[len], long total_evts=0){
 
     //check that they've input total_evts correctly
     for (int i=0;i<len;i++){
@@ -19,33 +18,19 @@ void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""
 	}
     }
 
-    //check that they've inpyt var correctly
-    if ((var=="") & (len>1)){
-	cout << "Are you varying PDE or LC?" << endl;
-	exit(EXIT_FAILURE);
-    }
-
     //Step 1: Plot the first file with title and axis labels
     //A: Read in the data file
-    double leak_energy, tot_good_nrg, erecoil, rec_psd;
+    double leak_energy, tot_good_nrg;
     int badPSD;
     constant_list constants;
     long evt_max;
-    double energy_min, energy_max, photo_yield, pde, coll_eff, pdes[len], coll_effs[len];
+    double energy_min, energy_max, pde, coll_eff, photo_yield;
     int tpb;
     TString evt_type, evt_types[len];
-    TCanvas *c1, *c2, *c3;
-    TH1D *hLeakEnergy;
-    TLegend *leg1, *leg2;
-    double fracs[len],fracErrs[len], zeros[len];
-    double fracErr;
-    TGraphErrors *ER = new TGraphErrors(); ER->SetLineColor(kRed); //stuff of the appropriate colour to add to the legend
-    TGraphErrors *NR = new TGraphErrors(); NR->SetLineColor(kBlack);
 
     TFile *fileIN = TFile::Open(files[0]);
     TTree *SiPMmc = (TTree*)fileIN->Get("SiPMmc");
-    SiPMmc->SetBranchAddress("rec_psd",&rec_psd);
-    SiPMmc->SetBranchAddress("erecoil",&erecoil);
+    SiPMmc->SetBranchAddress("leak_energy",&leak_energy);
     SiPMmc->SetBranchAddress("tot_good_nrg",&tot_good_nrg);
     SiPMmc->SetBranchAddress("badPSD",&badPSD);
     SiPMmc->SetBranchAddress("constants", (Long64_t*)(&constants));
@@ -61,8 +46,6 @@ void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""
     SiPMmc->GetEntry(7); evt_type = constants.recoil;
 
     evt_types[0] = evt_type;
-    pdes[0] = pde;
-    coll_effs[0] = coll_eff;
 
     //B: format
     //change constants into strings
@@ -75,78 +58,80 @@ void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""
 
     //get rid of the stats box
     gStyle->SetOptStat(0);
-    Color_t colors[14] = {kRed,kYellow+1,kBlue,kGray+1,kMagenta,kBlack,kGreen,kOrange+7,kViolet-3,kOrange+4,kGreen+4,kCyan,kRed+4,kCyan+4};
+    Color_t colors[14] = {kBlue,kYellow+1,kRed,kGreen,kOrange+10,kBlack,kMagenta,kGray+3,kViolet+3,kOrange+2,kGreen+4,kCyan,kRed+4,kCyan+4};
 
 
     //C: graphing
+    //Graph1: leakage vs energy
     double numbins = (energy_max-energy_min)*2.0;
     TH1D *hLeak = new TH1D("hLeak","",numbins,energy_min,energy_max);
     TH1D *hEnergy = new TH1D("hEnergy","",numbins,energy_min,energy_max);
-
-    double nr50;
-    //loop through all of the events and add them to the graphs
     for (int i=0;i<total_evts;i++){
 	SiPMmc->GetEntry(i);
-	nr50 = exp_fit(&erecoil,pars);
-	if (rec_psd>nr50 & rec_psd!=0 & rec_psd!=1) leak_energy = erecoil;
-	else leak_energy = -1;
 	hLeak->Fill(leak_energy);
 	hEnergy->Fill(tot_good_nrg);
     }
-
-    //Graph1: leakage vs energy
-    c1 = new TCanvas("c1", "c1");
-    c1->SetLogy();
+    TCanvas *c1 = new TCanvas("c1", "c1");
     hLeak->Sumw2();
     hEnergy->Sumw2();
-    hLeakEnergy = new TH1D(*hLeak);
-    if (len>1){
-      hLeakEnergy->SetMaximum(0.3);
-      hLeakEnergy->SetMinimum(0.0000001);
-    }
+    TH1D *hLeakEnergy = new TH1D(*hLeak);
     hLeakEnergy->Divide(hEnergy);
     hLeakEnergy->GetYaxis()->SetLimits(0,1);
     hLeakEnergy->SetLineColor(colors[0]);
+    hLeakEnergy->SetTitle("Events: "+num+", TPB: "+OnOff+", Type: "+evt_type+"R");
     hLeakEnergy->GetXaxis()->SetTitle("Recoil energy (keV)");
     hLeakEnergy->GetYaxis()->SetTitle("Leakage");
-    if (var=="PDE"){
-	hLeakEnergy->SetTitle("Events: "+num+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-	leg1 = new TLegend(.65,.89-0.03*len,.89,.89);
-	leg1->AddEntry(hLeakEnergy, "PDE: "+name_pde, "L");
-    }
-    else if (var=="LC"){
-	hLeakEnergy->SetTitle("Events: "+num+", PDE: "+name_pde+", TPB: "+OnOff+", Type: "+evt_type+"R");
-	leg1 = new TLegend(.12, .12, .35, .12+0.03*len);
-	leg1->AddEntry(hLeakEnergy, " LC: "+name_coll_eff, "L");
-    }
-    else if (var==""){
-	hLeakEnergy->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-	leg1 = new TLegend(0.,0.,0.,0.);
-    }
-    if (evt_type=='E'){
-	hLeakEnergy->Draw("E1");
-	leg1->Draw("same");
-    }
+    hLeakEnergy->Draw("E1");
+    TLegend *leg1 = new TLegend(.5,.78,.87,.88);
+    leg1->AddEntry(hLeakEnergy, "PDE: "+name_pde+" LC: "+name_coll_eff, "L");
+    leg1->Draw("same");
 
-    //Graph2 & Graph3: fracBad vs pde/coll_eff
+    //Graph2: fracBad vs pde
+    TCanvas *c2 = new TCanvas("c2", "c2");
     double fracBad = double(badPSD)/double(total_evts);
-    if (badPSD != 0) fracErr = fracBad*sqrt(1/badPSD + 1/total_evts);
-    else fracErr = 0;
-    fracs[0] = fracBad;
-    fracErrs[0] = fracErr;
-    zeros[0] = 0.0;
-//I want the ER events to be red and the NR to be black. Maybe put them in separate vectors?
+    double fracErr = fracBad*sqrt(1/badPSD + 1/total_evts);
+    TGraphErrors *badPDE = new TGraphErrors(1, &pde, &fracBad, 0, &fracErr);
+    if (evt_type=='E') badPDE->SetLineColor(kRed);
+    else if (evt_type=='N') badPDE->SetLineColor(kBlack);
+    badPDE->SetTitle("Events: "+num+", TPB: "+OnOff);
+    badPDE->GetXaxis()->SetLimits(0, 0.6);
+//why is this not working?
+    badPDE->GetYaxis()->SetLimits(0, 1);
+    badPDE->GetXaxis()->SetTitle("PDE");
+    badPDE->GetYaxis()->SetTitle("fraction of events with poorly reconstructed PSD");
+//why is the point not showing?
+    badPDE->Draw("APE");
+    TGraphErrors *ER = new TGraphErrors(); ER->SetLineColor(kRed); //stuff of the appropriate colour to add to the legend
+    TGraphErrors *NR = new TGraphErrors(); NR->SetLineColor(kBlack);
+    TLegend *leg2 = new TLegend(.70,.75,.87,.88);
+    leg2->AddEntry(ER, "ER", "L");
+    leg2->AddEntry(NR, "NR", "L");
+    leg2->Draw("same");
+
+    //Graph3: fracBad vs coll_eff
+    TCanvas *c3 = new TCanvas("c3", "c3");
+    TGraphErrors *badLC = new TGraphErrors(1, &coll_eff, &fracBad, 0, &fracErr);
+    if (evt_type=='E') badLC->SetLineColor(kRed);
+    else if (evt_type=='N') badLC->SetLineColor(kBlack);
+    badLC->SetTitle("Events: "+num+", TPB: "+OnOff);
+    badLC->GetXaxis()->SetLimits(0,1);
+//why is this not working?
+    badLC->GetYaxis()->SetLimits(0,1);
+    badLC->GetXaxis()->SetTitle("Collection Efficiency");
+    badLC->GetYaxis()->SetTitle("fraction of events with poorly reconstructed PSD");
+//why is the point not showing?
+    badLC->Draw("APE");
+    leg2->Draw("same");
+
 
     //Step 2: read and plot the rest of the files
     for (int i=1;i<len;i++){
 	//A: Read in the data file
 	fileIN = TFile::Open(files[i]);
 	SiPMmc = (TTree*)fileIN->Get("SiPMmc");
-//	SiPMmc->SetBranchAddress("leak_energy",&leak_energy);
-	SiPMmc->SetBranchAddress("erecoil",&erecoil);
+	SiPMmc->SetBranchAddress("leak_energy",&leak_energy);
 	SiPMmc->SetBranchAddress("tot_good_nrg",&tot_good_nrg);
 	SiPMmc->SetBranchAddress("badPSD",&badPSD);
-	SiPMmc->SetBranchAddress("rec_psd",&rec_psd);
 	SiPMmc->SetBranchAddress("constants", (Long64_t*)(&constants));
 
 	SiPMmc->GetEntry(0); evt_max = constants.evts;
@@ -160,20 +145,13 @@ void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""
 	SiPMmc->GetEntry(7); evt_type = constants.recoil;
 
 	evt_types[i] = evt_type;
-	pdes[i] = pde;
-	coll_effs[i] = coll_eff;
-
-	//check that the files have the same event type
-	if (evt_types[i] != evt_types[i-1]){
-		cout << "Files should have the same event type." << endl;
-		exit(EXIT_FAILURE);
-	}
 
 	//B: format
 	//change constants into strings
-	num = Form("%ld",total_evts);
-	name_pde = Form("%fd",pde);
-	name_coll_eff = Form("%fd",coll_eff);
+	TString num = Form("%ld",total_evts);
+	TString name_pde = Form("%fd",pde);
+	TString name_coll_eff = Form("%fd",coll_eff);
+	string OnOff;
 	if (tpb==0){OnOff="off";}
 	else if (tpb==1){OnOff="on";}
 
@@ -183,11 +161,8 @@ void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""
 	hEnergy = new TH1D("hEnergy","",numbins,energy_min,energy_max);
 	for (int i=0;i<total_evts;i++){
 	    SiPMmc->GetEntry(i);
-            nr50 = exp_fit(&erecoil,pars);
-            if (rec_psd>nr50 & rec_psd!=0 & rec_psd!=1) leak_energy = erecoil;
-            else leak_energy = -1;
-            hLeak->Fill(leak_energy);
-            hEnergy->Fill(tot_good_nrg);
+	    hLeak->Fill(leak_energy);
+	    hEnergy->Fill(tot_good_nrg);
 	}
 	c1->cd();
 	hLeak->Sumw2();
@@ -195,85 +170,31 @@ void ConstCompare(int len, TString files[len], long total_evts=0, TString var=""
 	hLeakEnergy = new TH1D(*hLeak);
 	hLeakEnergy->Divide(hEnergy);
 	hLeakEnergy->SetLineColor(colors[i]);
-	if (evt_type=='E'){
-	    hLeakEnergy->Draw("E1same");
-	    if (var=="PDE") leg1->AddEntry(hLeakEnergy, "PDE: "+name_pde, "L");
-	    else if (var=="LC") leg1->AddEntry(hLeakEnergy, " LC: "+name_coll_eff, "L");
-	    leg1->Draw("same");
-	}
+	hLeakEnergy->Draw("E1same");
+	leg1->AddEntry(hLeakEnergy, "PDE: "+name_pde+" LC: "+name_coll_eff, "L");
+	leg1->Draw("same");
 
-	//Graph2 & Graph3: fracBad vs PDE/coll_eff
+	//Graph2: fracBad vs PDE
+	c2->cd();
 	fracBad = double(badPSD)/double(total_evts);
-	if (badPSD != 0) fracErr = fracBad*sqrt(1/badPSD + 1/total_evts);
-	else fracErr = 0;
- 	fracs[i] = fracBad;
-	fracErrs[i] = fracErr;
-	zeros[i] = 0.0;
+	fracErr = fracBad*sqrt(1/badPSD + 1/total_evts);
+	badPDE = new TGraphErrors(1, &pde, &fracBad, 0, &fracErr);
+	if (evt_type=='E') badPDE->SetLineColor(kRed);
+	else if (evt_type=='N') badPDE->SetLineColor(kBlack);
+	badPDE->Draw("APE");
 
+	//Graph3: fracBad vs coll_eff
+	c3->cd();
+	badLC = new TGraphErrors(1, &coll_eff, &fracBad, 0, &fracErr);
+	if (evt_type=='E') badLC->SetLineColor(kRed);
+	else if (evt_type=='N') badLC->SetLineColor(kBlack);
+	badLC->Draw("APE");
     }
-
-
-    //Make graph2 and graph3
-    //Graph2: fracBad vs pde
-    c2 = new TCanvas("c2", "c2");
-    for (int i=0;i<len;i++) if (fracs[i] != 0){
-        c2->SetLogy();
-        break;
-    }
-    TGraphErrors *fracPDE = new TGraphErrors(len, pdes, fracs, zeros, fracErrs);
-    fracPDE->SetTitle("Events: "+num+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-    fracPDE->GetXaxis()->SetTitle("PDE");
-    fracPDE->GetYaxis()->SetTitle("fraction of events with poorly reconstructed PSD");
-    fracPDE->SetMarkerStyle(kFullCircle);
-    fracPDE->Draw("APE");
-//    leg2 = new TLegend(.70,.75,.87,.88);
-//    leg2->AddEntry(ER, "ER", "L");
-//    leg2->AddEntry(NR, "NR", "L");
-//    leg2->Draw("same");
-
-    //Graph3: fracBad vs coll_eff
-    c3 = new TCanvas("c3", "c3");
-    for (int i=0;i<len;i++) if (fracs[i] != 0){
-	c3->SetLogy();
-	break;
-    }
-    TGraphErrors *fracLC = new TGraphErrors(len, coll_effs, fracs, zeros, fracErrs);
-    fracLC->SetTitle("Events: "+num+", PDE: "+name_pde+", TPB: "+OnOff+", Type: "+evt_type+"R");
-    fracLC->GetXaxis()->SetLimits(0,1);
-    fracLC->GetYaxis()->SetLimits(0,1);
-    fracLC->GetXaxis()->SetTitle("Collection Efficiency");
-    fracLC->GetYaxis()->SetTitle("fraction of events with poorly reconstructed PSD");
-    fracLC->SetMarkerStyle(kFullCircle);
-    fracLC->Draw("APE");
-//    leg2->Draw("same");
 
 
     //Step 3: save stuff
-
-    //figure out if pde or coll_eff was varied
-//    int var_pde = 0, var_coll_eff = 0;
-//    if (len>1){
-//	for (int i=1;i<len;i++) if (pdes[i] != pdes[i-1]){
-//	    var_pde = 1;
-//	    break;
-//	}
-//	for (int i=1;i<len;i++) if (coll_effs[i] != coll_effs[i-1]){
-//	    var_coll_eff = 1;
-//	    break;
-//	}
-//    }
-    TString var_string = "";
-//    if (var_pde==1 & var_coll_eff==0) var = "_varPDE";
-//    else if (var_coll_eff==1 & var_pde==0) var = "_varLC";
-    if (var=="PDE") var_string = "_varPDE";
-    if (var=="LC") var_string = "_varLC";
-
-    //check how many ER files there were
-    int ERs = 0;
-    for (int i=0;i<len;i++) if (evt_types[i]=='E') ERs +=1;
-
-    gSystem->Exec("mkdir Img/ConstCompare/"+num);
-    if (ERs > 0) c1->SaveAs("Img/ConstCompare/"+num+"/TPB"+OnOff+"_files"+ERs+var_string+"__LeakageVsEnergy.png");
-    c2->SaveAs("Img/ConstCompare/"+num+"/TPB"+OnOff+"_files"+len+var_string+"__badPSDvsPDE.png");
-    c3->SaveAs("Img/ConstCompare/"+num+"/TPB"+OnOff+"_files"+len+var_string+"__badPSDvsLC.png");
+    gSystem->Exec("mkdir Img/ConstCompare");
+    if (find(evt_types, evt_types+len, 'E')<evt_types+len) c1->SaveAs("Img/ConstCompare/Evts:"+num+"TPB:"+OnOff+"__LeakageVsEnergy.png");
+    c2->SaveAs("Img/ConstCompare/Evts:"+num+"TPB:"+OnOff+"__badPSDvsPDE.png");
+    c3->SaveAs("Img/ConstCompare/Evts:"+num+"TPB:"+OnOff+"__badPSDvsLC.png");
 }
--- SiPMMC/ReadOutputNew.c
+++ SiPMMC/ReadOutputNew.c
@@ -2,12 +2,10 @@
 //TO RUN: define a TString array in the root terminal, then pass that into the function. (I don't know why it doesn't work otherwise)
 
 #include "MakeList.c"
-//#include "SingletToTripletYALE.c"
+#include "SingletToTripletYALE.c"
 #include "GetFloatAsString.c"
-//#include <math.h>
-# include "nr50.c"
 
-void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=0){
+void ReadOutputNew(int len, TString files[len], int heat_map=0, long total_evts=0){
 
     //check that they've input total_evts correctly
     for (int i=0;i<len;i++){
@@ -15,12 +13,6 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
             cout << "If the data file is a combination of multiple simulations, you must set total_evts." << endl;
             exit(EXIT_FAILURE);
         }
-    }
-
-    //check that files are in the correct order
-    if ((len==2) & files[0].Contains("NR") * files[1].Contains("ER")){
-	cout << "If inputting both ER and NR, ER should be first." << endl;
-	exit(EXIT_FAILURE);
     }
 
     //Step 1: Plot the first file with title and axis labels
@@ -86,14 +78,11 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
 
 
     //C: graphing
-    TH2D *hPSD = new TH2D("hPSD","",100,-0.02,1.02,100,0.,1.); //Graph1
+    TH2D *hPSD = new TH2D("hPSD","",100,-0.02,1.02,100,0,1); //Graph1
     double max_ph = energy_max*photo_yield*coll_eff;
-    TH2D *hPhotons = new TH2D("hphotons", "", 100, energy_min, energy_max, 100, 0., max_ph+50.); //Graph2
+    TH2D *hPhotons = new TH2D("hphotons", "", 100, energy_min, energy_max, 100, 0, max_ph+50); //Graph2
     TH2D *hPSDenergy = new TH2D("hPSDenergy", "", 100, energy_min, energy_max, 100, -0.02, 1.02); //Graph3
-    TH1D *hRes = new TH1D("hRes","",100, 0., 1.); //Graph4
-//    long height = 14512.68*pow(coll_eff,0.5623)*pow(pde,0.4819)*(0.072*total_evts + 40.994);
-    long height = (2590*coll_eff)*(7300*pde)*(0.158*total_evts)/4200000;
-    if (len==2) hRes->SetMaximum(height);
+    TH1D *hRes = new TH1D("hRes","",100, 0, 1); //Graph4
     double numbins = (energy_max-energy_min)*2.0;
     TH1D *hLeak = new TH1D("hLeak","",numbins,energy_min,energy_max); //Graph5
     TH1D *hEnergy = new TH1D("hEnergy","",numbins,energy_min,energy_max); //Graph5
@@ -113,13 +102,12 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
     TGraphErrors *ER = new TGraphErrors(); ER->SetFillColor(kRed); //stuff of the appropriate colour to add to the legend
     TGraphErrors *NR = new TGraphErrors(); NR->SetFillColor(kBlack);
     TCanvas *c1, *c2, *c3, *c4, *c5, *c6;
-    TLegend *leg1=new TLegend(0.,0.,0.,0.), *leg3=new TLegend(0.,0.,0.,0.), *leg4=new TLegend(0.,0.,0.,0.);
+    TLegend *leg1, *leg3, *leg4;
     TF1 *line1;
-//    TGraph *line3;
-    TF1 *line3;
+    TGraph *line3;
     TH1D *hLeakEnergy;
-    double fracBad;
     TString name_fracBad;
+    double fracBad;
 
     //Graph1: true PSD vs recorded PSD
     //if ER/NR comparison
@@ -129,32 +117,28 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
     if (evt_type=='E') hPSD->SetMarkerColor(kRed);
     if (len==1){
 	hPSD->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-        if (evt_type=='N'){
-	    hPSDenergy->SetMarkerColor(kBlack);
-	    if (heat_map==1){
-		gStyle->SetPalette(kPigeon);
-		TColor::InvertPalette();
-	    }
-	}
+        if (evt_type=='N') hPSDenergy->SetMarkerColor(kBlack);
+	if ((heat_map==1) & (evt_type=='N')) gStyle->SetPalette(kPigeon);
     }
     else { //len==2
 	hPSD->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff);
-        if (evt_type=='N') hPSD->SetMarkerColorAlpha(kBlack, 0.5); //not working to make it translucent
+        if (evt_type=='N') hPSDenergy->SetMarkerColorAlpha(kBlack, 0.5); //not working
+	//legend
 	leg1 = new TLegend(.13,.68,.30,.88);
 	leg1->AddEntry(ER, "ER", "F");
 	leg1->AddEntry(NR, "NR", "F");
+	leg1->Draw();
     }
     if (heat_map==1){
-	if (evt_type=='E'){
-	    gStyle->SetPalette(kCherry);
-	    TColor::InvertPalette();
-	}
-	hPSD->Draw("COLZ");
+	if (evt_type=='E') gStyle->SetPalette(kCherry);
+	TColor::InvertPalette();
+	hPSD->Draw("COLZ same");
     }
-    else hPSD->Draw();
+    else hPSD->Draw("same"); //heat_map==0
     //add a y=x line
     line1 = new TF1("line","x",-0.02,1.02);
     line1->SetLineColor(kBlue);
+    line1->Draw("same");
 
     //Graph2: detected photons vs energy
     //if ER/NR comparison
@@ -164,26 +148,25 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
     if (evt_type=='E') hPhotons->SetMarkerColor(kRed);
     else hPSD->SetMarkerColor(kBlack); //evt_type=='N'
     if (len==1)	hPhotons->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-    else {//len==2
+    else {
 	hPhotons->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff); //len==2
+	leg1->Draw();
     }
-    hPhotons->Draw();
+    hPhotons->Draw("same");
 
     //Graph3: recorded PSD vs energy
     //if ER/NR comparison
     c3 = new TCanvas("c3","c3");
     //add a SingletToTripletYale line; it's stepped because that's how I implemented the leakage cut.
-//    double energyLine[2*size];
-//    double nuclearLine[2*size];
-//    for (int i=0;i<size;i++){
-//        energyLine[2*i] = energy[i]-0.5;
-//        energyLine[2*i + 1] = energy[i]+0.5;
-//        nuclearLine[2*i] = nuclear[i];
-//        nuclearLine[2*i + 1] = nuclear[i];
-//    }
-//    line3 = new TGraph(2*size, energyLine, nuclearLine);
-    line3 = new TF1("nr50",exp_fit,energy_min,energy_max,4);
-    for (int i=0;i<4;i++) line3->SetParameter(i,pars[i]);
+    double energyLine[2*size];
+    double nuclearLine[2*size];
+    for (int i=0;i<size;i++){
+        energyLine[2*i] = energy[i]-0.5;
+        energyLine[2*i + 1] = energy[i]+0.5;
+        nuclearLine[2*i] = nuclear[i];
+        nuclearLine[2*i + 1] = nuclear[i];
+    }
+    line3 = new TGraph(2*size, energyLine, nuclearLine);
     line3->SetLineColor(kBlue);
     line3->SetLineWidth(3);
     hPSDenergy->GetXaxis()->SetTitle("Recoil energy (keV)");
@@ -191,10 +174,8 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
     if (evt_type=='E') hPSDenergy->SetMarkerColor(kRed);
     if (len==1){
         hPSDenergy->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-	if (evt_type=='N'){
-	    hPSDenergy->SetMarkerColor(kBlack);
-            if (heat_map==1) gStyle->SetPalette(kPigeon);
-	}
+	if (evt_type=='N') hPSDenergy->SetMarkerColor(kBlack);
+        if ((heat_map==1) & (evt_type=='N')) gStyle->SetPalette(kPigeon);
     }
     else { //len==2
         hPSDenergy->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff);
@@ -204,19 +185,18 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
         leg3->AddEntry(ER, "ER", "F");
         leg3->AddEntry(NR, "NR", "F");
 	leg3->AddEntry(line3, "50% NR", "L");
+        leg3->Draw();
     }
     if (heat_map==1){
         if (evt_type=='E') gStyle->SetPalette(kCherry);
-        hPSDenergy->Draw("COLZ");
+        hPSDenergy->Draw("COLZ same");
     }
-    else hPSDenergy->Draw();
+    else hPSDenergy->Draw("same"); //heat_map==0
+
+    line3->Draw("same");
 
     //Graph4: histogram of residual
     c4 = new TCanvas("c4", "c4");
-    //calculate height of NR peak
-//    long height = 14512.68*pow(coll_eff,0.5623)*pow(pde,0.4819)*(0.072*total_evts + 40.994);
-//    hRes->GetYaxis()->SetLimits(0,height);
-//    hRes->SetMaximum(height);
     hRes->GetXaxis()->SetTitle("PSD Residual");
     hRes->GetYaxis()->SetTitle("number of events");
     if (evt_type=='E') hRes->SetLineColor(kRed);
@@ -227,13 +207,13 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
 	leg4 = new TLegend(.88,.72,.74,.89);
 	leg4->AddEntry(ER, "ER", "F");
 	leg4->AddEntry(NR, "NR", "F");
+	leg4->Draw("same");
     }
-    hRes->Draw();
+    hRes->Draw("same");
 
     //Graph5: leakage vs energy
     c5 = new TCanvas("c5", "c5");
     if (evt_type=='E'){
-	c5->SetLogy();
 	//calculate error bars
 	hLeak->Sumw2();
 	hEnergy->Sumw2();
@@ -248,14 +228,14 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
     //Canvas6: Fraction of events with PSD zero or 1
     c6 = new TCanvas("c6", "c6");
     fracBad = double(badPSD)/double(total_evts);
-    name_fracBad = Form("%fd", fracBad);
+    name_fracBad = GetFloatAsString(fracBad);
     cout << "The fraction of events with PSD zero or one" << endl;
     cout<<"Events: "<<total_evts<<", PDE: "<<pde<<", Collection Efficiency: "<<coll_eff<<", TPB: "<<OnOff<<", Type: "<<evt_type<<"R, fracBad: "<<fracBad<<endl;
-    TLatex *text1 = new TLatex(0.02, 0.9, "The fraction of events with PSD zero or one");
-    text1->Draw();
-    TLatex *text2 = new TLatex(0.02, 0.8, "Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R, fracBad: "+name_fracBad);
-    text2->SetTextSize(0.03);
-    text2->Draw("same");
+//why isn't this showing up?
+    TLatex text1(0.25, 0.9, "The fraction of ER events with PSD zero or one");
+    text1.Draw();
+    TLatex text2(0.25, 0.8, "Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R, fracBad: "+name_fracBad);
+    text2.Draw("same");
 
     //graphs from ConstCompare.c
 
@@ -336,9 +316,8 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
 	c3->cd();
 	if (evt_type=='E') hPSDenergy->SetMarkerColor(kRed);
 	else if (evt_type=='N') hPSDenergy->SetMarkerColorAlpha(kBlack, 0.5); //not working
-	if ((heat_map==1) & evt_type=='E'){
-	    gStyle->SetPalette(kCherry);
-	    TColor::InvertPalette();
+	if (heat_map==1){
+	    if (evt_type=='E') gStyle->SetPalette(kCherry);
 	    hPSDenergy->Draw("COLZ same");
 	}
 	else hPSDenergy->Draw("same"); //heat_map==0
@@ -356,37 +335,21 @@ void ReadOutputNew(int len, TString files[len], long total_evts=0, int heat_map=
 	    hEnergy->Sumw2();
 	    hLeakEnergy = new TH1D(*hLeak);
 	    hLeakEnergy->Divide(hEnergy);
-	    hLeakEnergy->SetTitle("Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R");
-	    hLeakEnergy->GetXaxis()->SetTitle("Recoil energy (keV)");
-	    hLeakEnergy->GetYaxis()->SetTitle("Leakage");
 	    hLeakEnergy->Draw("E1 same");
 	}
 
 	//Canvas6: Fraction of events with PSD zero or 1
 	c6->cd();
 	fracBad = double(badPSD)/double(total_evts);
-	name_fracBad = Form("%fd", fracBad);
+	name_fracBad = GetFloatAsString(fracBad);
 	cout<<"Events: "<<total_evts<<", PDE: "<<pde<<", Collection Efficiency: "<<coll_eff<<", TPB: "<<OnOff<<", Type: "<<evt_type<<"R, fracBad: "<<fracBad<<endl;
-	text2 = new TLatex(0.02, 0.8-(0.1*i), "Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R, fracBad: "+name_fracBad);
-	text2->SetTextSize(0.03);
-	text2->Draw("same");
+//why isn't this showing up?
+	TLatex text2(0.25, 0.8-(0.1*i), "Events: "+num+", PDE: "+name_pde+", Collection Efficiency: "+name_coll_eff+", TPB: "+OnOff+", Type: "+evt_type+"R, fracBad: "+name_fracBad);
+	text2.Draw("same");
 
 	//graphs from ConstCompare
 
     }
-
-    //draw lines and legends last so that they're on top
-    c1->cd();
-    leg1->Draw("same");
-    line1->Draw("same");
-    c2->cd();
-    leg1->Draw("same");
-    c3->cd();
-    leg3->Draw("same");
-    line3->Draw("same");
-    c4->cd();
-    leg4->Draw("same");
-
 
     //Step 3: save stuff
     gSystem->Exec("mkdir Img/"+directory);
